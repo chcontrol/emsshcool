6b43aa743e3d827c22c9e8b7f6c6063e4d2f251b
peeraphut <p.peeraphut@gmail.com>
2020-06-23 12:59:25 +0700

    เพิ่มข้อมูล รายละเอียดผู้สอบหน้า admin เอาผ่าน/ไม่ผ่าน ออก

--- app/modules/admin/views/show.php
+++ app/modules/admin/views/show.php
@@ -1,37 +1,50 @@
 <?php $this->load->view('admin/header'); ?>
-<style media="print">    
-    .no-print{
+<style media="print">
+    .no-print {
         display: none;
     }
 </style>
-    <div class="wrapper profile-page">
-        <div class="page-header header-filter header-small" data-parallax="active" style="background-image: url('<?php echo base_url('static/img/cover.jpg'); ?>');"></div>
-        <div class="main main-raised">
+<div class="wrapper profile-page">
+    <div class="page-header header-filter header-small" data-parallax="active" style="background-image: url('<?php echo base_url('static/img/cover.jpg'); ?>');"></div>
+    <div class="main main-raised">
         <div class="profile-content">
             <div class="container">
                 <div class="row" class="no-print">
                     <div class="col-xs-6 col-xs-offset-3">
-                       <div class="profile">
+                        <div class="profile">
                             <div class="avatar">
-                            <?php
-                                if($user->fb_id !=''){
-                                    $avatar = 'https://graph.facebook.com/'.$user->fb_id.'/picture?height=300&width=300';
-                                }else{
+                                <?php
+                                if ($user->fb_id != '') {
+                                    $avatar = 'https://graph.facebook.com/' . $user->fb_id . '/picture?height=300&width=300';
+                                } else {
                                     $avatar = base_url('static/img/faces/christian.jpg');
                                 }
-                            ?>
+                                ?>
                                 <img src="<?php echo $avatar; ?>" alt="Circle Image" class="img-circle img-responsive img-raised no-print">
                             </div>
+
                             <div class="name">
-                                <h3 class="title"><?php echo $user->name ?></h3>                              
+                                <h2 class="title text-center">ผลคะแนนสอบ <br>
+                                </h2>
+                                <h3 class="title">
+                                    คุณ
+                                    <?php echo $user->name ?>
+                                    <br>
+                                    ชั้น
+                                    <?php echo $user->class_room ?>
+                                    ห้อง
+                                    <?php echo $user->room_no ?>
+                                    โรงเรียน
+                                    <?php echo $user->school ?>
+                                </h3>
                             </div>
                         </div>
-                    </div>                    
-                </div>                
+                    </div>
+                </div>
                 <div class="row">
-                    <h2 class="title text-center">ผลคะแนนสอบ</h2>
+
                     <!-- <div class="text-center">
-                        <a target="_blank" href="<?php echo base_url('admin/users/'.$user->id.'/print')?>"><button class="btn">PRINT</button></a><br/>
+                        <a target="_blank" href="<?php echo base_url('admin/users/' . $user->id . '/print') ?>"><button class="btn">PRINT</button></a><br/>
                     </div> -->
                     <legend></legend>
                     <div class="col-md-8 col-md-offset-2">
@@ -40,49 +53,49 @@
                                 <thead>
                                     <tr>
                                         <th class="text-center">#</th>
-                                        <th>ประเภท</th>                                                                                
-                                        <th>ชื่อ</th>                                                                                
+                                        <th>ประเภท</th>
+                                        <th>ชื่อ</th>
                                         <th>คะแนน</th>
-                                        <th class="text-right">ผลการสอบ</th>
+                                        <th hidden class="text-right">ผลการสอบ</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <?php
-                                        $i = 1; 
-                                        $lessons = ['การแจ้งเหตุ 1669','การปฐมพยาบาลเบื้องต้น','การช่วยฟื้นคืนชีพเบื้องต้น','EMS Rally'];
-                                        foreach($objs as $row): 
+                                    $i = 1;
+                                    $lessons = ['การแจ้งเหตุ 1669', 'การปฐมพยาบาลเบื้องต้น', 'การช่วยฟื้นคืนชีพเบื้องต้น', 'EMS Rally'];
+                                    foreach ($objs as $row) :
                                     ?>
-                                    <tr>
-                                        <td class="text-center"><?php echo $i++; ?></td>
-                                        <td>
-                                            <?php if($row->status == 1): ?>
-                                                สอบก่อนเรียน
-                                            <?php else: ?>
-                                                สอบหลังเรียน
-                                            <?php endif; ?>
-                                        </td>
-                                        <td>
-                                            <?php echo $lessons[$row->exam_id-1]; ?>
-                                        </td>
-                                        <td><?php echo $row->score?>/10</td>
-                                        <td class="td-actions text-right">
-                                            <?php if($row->score >=5 ): ?>
-                                                ผ่าน
-                                            <?php else: ?>
-                                                ไม่ผ่าน
-                                            <?php endif; ?>
-                                        </td>
-                                    </tr>
+                                        <tr>
+                                            <td class="text-center"><?php echo $i++; ?></td>
+                                            <td>
+                                                <?php if ($row->status == 1) : ?>
+                                                    สอบก่อนเรียน
+                                                <?php else : ?>
+                                                    สอบหลังเรียน
+                                                <?php endif; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $lessons[$row->exam_id - 1]; ?>
+                                            </td>
+                                            <td><?php echo $row->score ?>/10</td>
+                                            <td hidden class="td-actions text-right">
+                                                <?php if ($row->score >= 5) : ?>
+                                                    ผ่าน
+                                                <?php else : ?>
+                                                    ไม่ผ่าน
+                                                <?php endif; ?>
+                                            </td>
+                                        </tr>
                                     <?php endforeach; ?>
                                 </tbody>
                             </table>
                             <div class="pull-right" style="margin-bottom:20px;">
-                                <a href="<?php echo base_url('admin/user-list')?>">ดูรายชื่อผู้เรียน</a>
+                                <a href="<?php echo base_url('admin/user-list') ?>">ดูรายชื่อผู้เรียน</a>
                             </div>
                         </div>
                     </div>
-                </div>                
+                </div>
             </div>
         </div>
     </div>
-<?php $this->load->view('site/footer'); ?>
+    <?php $this->load->view('site/footer'); ?>
